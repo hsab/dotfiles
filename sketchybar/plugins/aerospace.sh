8480667bd15e7bb89fcf2cab0caa97ef80a52e83
hsab <hsab@users.noreply.github.com>
2024-12-19 13:22:10 -0800

    border sb<->aerospace

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -3,8 +3,10 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.border_width=2 background.border_color=0xFFFFFFFF icon.color=0xFFFFFFFF label.color=0xFFFFFFFF
-else
-  sketchybar --set $NAME  background.border_width=1 background.border_color=0x88FFFFFF icon.color=0x88FFFFFF label.color=0x88FFFFFF
+if [ "$SENDER" = "aerospace_workspace_change" ]; then
+  if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set $NAME background.border_width=2 background.border_color=0xFFFFFFFF icon.color=0xFFFFFFFF label.color=0xFFFFFFFF
+  else
+    sketchybar --set $NAME background.border_width=1 background.border_color=0x88FFFFFF icon.color=0x88FFFFFF label.color=0x88FFFFFF
+  fi
 fi
--- sketchybar/plugins/space_windows.sh
+++ sketchybar/plugins/space_windows.sh
@@ -4,18 +4,21 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
   prevapps=$(aerospace list-windows --workspace "$PREV_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
   if [ "${prevapps}" != "" ]; then
     sketchybar --set space.$PREV_WORKSPACE drawing=on
-    icon_strip=" "
-    while read -r app
-    do
-      icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-    done <<< "${prevapps}"
-    sketchybar --set space.$PREV_WORKSPACE label="$icon_strip"
   else
+    # if there are no apps in the previous workspace, hide the space
     sketchybar --set space.$PREV_WORKSPACE drawing=off
   fi
+fi
 
-  apps=$(aerospace list-windows --workspace "$FOCUSED_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-  sketchybar --set space.$FOCUSED_WORKSPACE drawing=on
+if [ "$SENDER" = "aerospace_workspace_change" ] || [ "$SENDER" = "space_windows_change" ] || [ "$SENDER" = "system_woke" ]; then
+  # get the focused workspace
+  focused_workspace=$(aerospace list-workspaces --focused)
+  # get the apps in the focused workspace
+  apps=$(aerospace list-windows --workspace "$focused_workspace" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+  # set the space to drawing=on
+  sketchybar --set space.$focused_workspace drawing=on
+
+  # create the icon strip
   icon_strip=" "
   if [ "${apps}" != "" ]; then
     while read -r app
@@ -25,5 +28,12 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
   else
     icon_strip=""
   fi
-  sketchybar --set space.$FOCUSED_WORKSPACE label="$icon_strip"
+
+
+  if [ "$icon_strip" != "" ]; then
+    sketchybar --set space.$focused_workspace label="$icon_strip" label.drawing=on
+    sketchybar --set space.$focused_workspace icon.padding_right=0
+  else
+    sketchybar --set space.$focused_workspace label.drawing=off icon.padding_right=12 
+  fi
 fi
