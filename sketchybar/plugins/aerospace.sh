2e357c1e3f444d83901b03155b37a86aa3bb5a3d
hsab <hsab@users.noreply.github.com>
2024-12-30 16:08:42 -0800

    new mode

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -10,3 +10,11 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
     sketchybar --set $NAME background.border_width=1 background.border_color=0x88FFFFFF icon.color=0x88FFFFFF label.color=0x88FFFFFF
   fi
 fi
+
+if [ "$SENDER" = "aerospace_mode_change" ]; then
+  if [ "$MODE" = "MAIN" ]; then
+    sketchybar --set "$NAME" label="M" drawing=off
+  else
+    sketchybar --set "$NAME" label="$MODE" background.color=0xFFdc2626 background.border_color=0xFFdc2626 drawing=on
+  fi
+fi
